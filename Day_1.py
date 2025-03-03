0382757a02e6932e009c6dea0d4a96cdbfb310dd
Mumraizb <mumraizbabar@gmail.com>
2025-03-03 21:46:12 +0500

    Day 1 learning

---
+++ Day_1.py
@@ -0,0 +1,35 @@
+#Fundamental data types
+
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+
+#Classes --> Custom types
+
+#Specialized Data Types
+
+None
+
+############ Fundamental Data Types
+
+print(type(2 + 4))
+print(type(2 - 4))
+print(type(2 / 4))
+print(type(2 * 4)) 
+
+# float takes a lot more memory space than int
+print(2 ** 4)
+
+print(5 // 2)
+
+#math functions
+print(round(3.4))
+print(abs(-44))
+
+
+
