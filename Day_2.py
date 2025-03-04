de6a9342644a87941925f33638e6cbfd6b2d22a8
Mumraizb <mumraizbabar@gmail.com>
2025-03-04 19:41:40 +0500

    Python practice

---
+++ Day_2.py
@@ -0,0 +1,28 @@
+#Operator precedence
+
+#print(20 - 3*4)
+
+#1. Brackets()
+#2. ** power
+#3. * /
+#4.//
+#5. + -
+
+#print((20-3) + 2 **3)
+
+print((5 + 4) * 10 / 2) #45
+
+print(((5 + 4) * 10) / 2) #45
+
+print((5 + 4) * (10 / 2)) #45
+
+print(5 + (4 * 10) / 2) #25
+
+print(5 + 4 * 10 // 2) #25
+
+print(45 // 2) 
+
+#binary
+
+print(bin(5))
+
